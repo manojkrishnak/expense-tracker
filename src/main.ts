6567a3987ac8b1a2429d7ff091c9ad74852b224b
manojkrishnak <manojkrishna11@gmail.com>
2022-10-16 23:19:21 +0530

    change port for app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose']
   });
-  await app.listen(3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
